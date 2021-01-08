dc75d953dcb9e5c4c60a0dcda1f9a39f5da8241d
yuyingsu <yuyingsujob@gmai.com>
2021-01-07 20:34:12 -0500

    second draft

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let colNum=1;
-let rowNum=1;
+let colNum=0;
+let rowNum=0;
 const addRow = () => {
+    if(colNum===0 && rowNum===0){
+      colNum=1;
+      rowNum=1;
+      let initial = document.createElement("div");
+      initial.id="column1_row1";
+      document.getElementById("grids").appendChild(initial);
+    }
+    else{
     let start = document.getElementById("column"+colNum+"_"+"row"+rowNum);
     for(let i=1;i<=colNum;i++){
       let newDiv=document.createElement('div');
@@ -12,8 +20,16 @@ const addRow = () => {
       start=newDiv;
     }
     rowNum++;
+   }
 }
 const addColumn = () => {
+  if(colNum===0 && rowNum===0){
+    colNum=1;
+    rowNum=1;
+    let initial = document.createElement("div");
+    initial.id="column1_row1";
+    document.getElementById("grids").appendChild(initial);
+  }else{
   for(let i=1;i<=rowNum;i++){
     let curr=document.getElementById("column"+colNum+"_"+"row"+i);
     let newDiv=document.createElement('div');
@@ -21,20 +37,31 @@ const addColumn = () => {
     curr.parentNode.insertBefore(newDiv, curr.nextSibling);
   }
   colNum++;
+  }
 }
 
 const removeColumn = () => {
+  if(rowNum!==0 && colNum!==0){
   for(let i=1;i<=rowNum;i++){
     let curr=document.getElementById("column"+colNum+"_"+"row"+i);
     curr.parentNode.removeChild(curr);
   }
   colNum--;
+  if(colNum===0){
+    rowNum=0;
+  }
+ }
 }
 
 const removeRow = () => {
+  if(rowNum!==0 && colNum!==0){
   for(let i=1;i<=colNum;i++){
     let curr=document.getElementById("column"+i+"_"+"row"+rowNum);
     curr.parentNode.removeChild(curr);
   }
   rowNum--;
+  if(rowNum===0){
+    colNum=0;
+  }
+ }
 }
